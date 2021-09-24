f254ecc36f1ae228c3576eb4c0ec797ba997b08a
Jim Ladd <jladd@redhat.com>
2021-09-23 21:00:48 -0700

    add ansible as a prereq

--- host/install_prereqs.sh
+++ host/install_prereqs.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
 sudo dnf install -y make
-sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo dnf install -y ansible
