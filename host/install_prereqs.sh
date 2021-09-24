5def6ff11dd1ea3661808d6fd24a915513a7d774
Jim Ladd <jladd@redhat.com>
2021-09-24 05:28:00 +0000

    install docker-ce as prereq

--- host/install_prereqs.sh
+++ host/install_prereqs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install -y docker-ce
+sudo usermod -a -G docker $USER
 sudo dnf install -y make
 sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo dnf install -y ansible
