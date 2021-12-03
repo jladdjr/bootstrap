e3442a5a01cac63b94fc69773f94079f6518fdb5
Jim Ladd <jladdjr@gmail.com>
2021-12-02 17:10:12 -0800

    use evil mode

--- debian-host/roles/dev-tools/files/emacs/init.el
+++ debian-host/roles/dev-tools/files/emacs/init.el
@@ -1,7 +1,3 @@
-;; Adjust keyboard layout for Dvoraks
-;; https://www.emacswiki.org/emacs/DvorakKeyboard
-
-(global-set-key (kbd "C-z") ctl-x-map)
-
-(global-set-key (kbd "C-x C-h") help-map)
-(global-set-key (kbd "C-h") 'previous-line)
+;; evil
+(require 'evil)
+(evil-mode 1)
