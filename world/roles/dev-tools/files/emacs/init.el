a7e287bf87dd1ecc8c63370dcf706ac07aa4079a
Jim Ladd <jim.ladd@finitestate.io>
2022-06-08 16:00:35 -0400

    use swiper for default search

--- world/roles/dev-tools/files/emacs/init.el
+++ world/roles/dev-tools/files/emacs/init.el
@@ -2961,3 +2961,6 @@ chicken and egg problem."
     (defalias #'forward-evil-word #'forward-evil-symbol)
     ;; make evil-search-word look for symbol rather than word boundaries
     (setq-default evil-symbol-word-search t))
+
+;; use swiper for default isearch
+(global-set-key "\C-s" 'swiper)
