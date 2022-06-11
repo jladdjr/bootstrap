d91bef772c4b32b3c8a22df44173e52f074b092c
Jim Ladd <jladdjr@gmail.com>
2022-06-11 14:21:18 -0400

    drop function that tried to implement context-based compose

--- world/roles/dev-tools/files/emacs/init.el
+++ world/roles/dev-tools/files/emacs/init.el
@@ -3029,27 +3029,5 @@ chicken and egg problem."
      ;; Include any other accounts here ...
     ))
 
-(defun my-mu4e-set-account ()
-  "Set the account for composing a message.
-   This function is taken from:
-     https://www.djcbsoftware.nl/code/mu/mu4e/Multiple-accounts.html"
-  (let* ((account
-    (if mu4e-compose-parent-message
-        (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
-    (string-match "/\\(.*?\\)/" maildir)
-    (match-string 1 maildir))
-      (completing-read (format "Compose with account: (%s) "
-             (mapconcat #'(lambda (var) (car var))
-            my-mu4e-account-alist "/"))
-           (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
-           nil t nil nil (caar my-mu4e-account-alist))))
-   (account-vars (cdr (assoc account my-mu4e-account-alist))))
-    (if account-vars
-  (mapc #'(lambda (var)
-      (set (car var) (cadr var)))
-        account-vars)
-      (error "No email account found"))))
-(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
-
 ;; global shortcut for mu4e
 (global-set-key (kbd "C-q") 'mu4e)
