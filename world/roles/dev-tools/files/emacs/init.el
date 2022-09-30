9bc27824fe229fe7fa8a314b66e5eeca76e5e86c
Jim Ladd <jladdjr@gmail.com>
2022-09-29 20:12:42 -0700

    make C-c C-c close eyebrowse window config for magit

--- world/roles/dev-tools/files/emacs/init.el
+++ world/roles/dev-tools/files/emacs/init.el
@@ -2828,6 +2828,7 @@ chicken and egg problem."
   (magit-status)
   (delete-other-windows))
 (define-key (current-global-map) [remap magit-status] 'jl/magit-status)
+(define-key magit-mode-map (kbd "C-c C-c") 'eyebrowse-close-window-config)
 
 ;; custom window switching
 (defun jl/split-window-below ()
