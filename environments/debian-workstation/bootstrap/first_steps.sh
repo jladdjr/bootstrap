ff4a19dfec9cb965afb3d5d36d11a3eca01c9d97
Jim Ladd <jladd@tuta.io>
2023-07-30 14:56:04 -0700

    update destination dir for copying pass, add notes for importing gpg key

--- environments/debian-workstation/bootstrap/first_steps.sh
+++ environments/debian-workstation/bootstrap/first_steps.sh
@@ -12,3 +12,14 @@ Welcome to your new Debian 12 system!
 
 Now install the basic utilities needed to proceed with boostrapping:
 `bash ./install_requirements.sh`
+
+## Install your gpg key
+
+Install key with:
+
+`gpg --import /path/to/key.asc`
+
+Next, configure the key's trust level with:
+
+`gpg --edit-key <key id>`
+followed by entering `trust`, hitting return, and selecting the trust level.
