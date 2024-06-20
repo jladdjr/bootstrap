b4212445f23fa34e86f53372b78a813db891dbfa
Jim Ladd <j@portladds.com>
2024-06-19 18:50:35 -0700

    Update backup.sh (for Go kits) to use bare Git repos
    Was running into issues where 'git pull' was refusing to update
    the branch on the USB stick because it thought that local files
    had been changed (even after attempting to reset those files).
    Bare repos don't complain about overwriting local checkouts : )
    
    Note: Still need to update initial_backup.sh

--- go-kit-usb-sticks/backup.sh
+++ go-kit-usb-sticks/backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 USB_DIR=/media/jim/GOKIT
 HOME_DIR=/home/jim
 
@@ -7,14 +9,25 @@ HOME_DIR=/home/jim
 # add --info=progress2 to see verbose output
 rsync -avz --delete ${HOME_DIR}/.ssh ${USB_DIR}
 
-# bootstrap
-cd ${USB_DIR}/bootstrap
-git pull -f
+# bootstrap (update bare repo)
+cd ${USB_DIR}/bootstrap.git
+git fetch ${HOME_DIR}/git/bootstrap/.git devel:devel
 
-# pass
-cd ${USB_DIR}/.password-store
-git pull -f ${HOME_DIR}/.password-store
+# pass (update bare repo)
+cd ${USB_DIR}/password-store.git
+git fetch ${HOME_DIR}/.password-store/.git monolith:monolith
 
 # bah
-cd ${USB_DIR}/bah/.password-store
-git pull -f ${HOME_DIR}/.bah/.password-store
+# cd ${USB_DIR}/bah/.password-store
+# git pull -f ${HOME_DIR}/.bah/.password-store
+
+################################################
+# How to convert regular git repo to bare repo #
+################################################
+# https://stackoverflow.com/questions/2199897/how-to-convert-a-normal-git-repository-to-a-bare-one
+
+# tldr:
+# - Rename .git folder inside regular git repo to repo.git
+# - Move repo.git up a folder
+# - Remove original repo's folder
+# - cd in repo.git and run 'git config --bool core.bare true'
--- go-kit-usb-sticks/initial_backup.sh
+++ go-kit-usb-sticks/initial_backup.sh
@@ -8,10 +8,10 @@ HOME_DIR=/home/jim
 #rsync -avz ${HOME_DIR}/.ssh ${USB_DIR}
 
 # bootstrap
-rsync -avz ${HOME_DIR}/git/bootstrap ${USB_DIR}
+rsync -avz ${HOME_DIR}/git/bootstrap/.git ${USB_DIR}/bootstrap.git
 
 # pass
-rsync -avz ${HOME_DIR}/.password-store ${USB_DIR}
+rsync -avz ${HOME_DIR}/.password-store/.git ${USB_DIR}/password-store.git
 
 # bah
-rsync -avz ${HOME_DIR}/.bah ${USB_DIR}
+rsync -avz ${HOME_DIR}/.bah/.git ${USB_DIR}/bah.git
